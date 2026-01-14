f31f7a0d4e788bc5b1f047085fc5bb54d6399064
Ekaterina Anosova <Zkat2008@yandex.ru>
2026-01-14 17:51:01 +0400

    Чистый код: настройка линтера и форматтера

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+pluginJs.configs.recommended,
+eslintPluginPrettierRecommended
+];
